125cc7b2e850160cefcedc34996604cd9c0f71b4
Gökhan Kılıç <gokhankilic@Gokhans-MacBook-Air.local>
2019-02-15 12:42:31 +0300

    Gif was added

---
+++ Pods/Target Support Files/Pods-MyWorkoutApp/Pods-MyWorkoutApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyWorkoutApp : NSObject
+@end
+@implementation PodsDummy_Pods_MyWorkoutApp
+@end
---
+++ Pods/Target Support Files/Pods-MyWorkoutAppTests/Pods-MyWorkoutAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyWorkoutAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyWorkoutAppTests
+@end
